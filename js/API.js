fe20901fb04e5a7ad3e74a835de5c0e9d48cac50
Yamil Escobar <joseyamilej@hotmail.com>
2022-09-22 22:57:52 +0200

    Eliminar Clientes de la API

--- js/API.js
+++ js/API.js
@@ -30,3 +30,15 @@ export const obtenerClientes = async() => {
     }
 
 }
+
+// Elimina un Cliente...
+
+export const eliminarCliente = async id => {
+    try {
+        await fetch (`${url}/${id}`, {
+            method: 'DELETE'
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
